dc8a8b8fdd29132f6bb34c4756461cc13443425e
Pedro Domingues Santos <pedrodominsan@gmail.com>
2025-11-24 05:40:56 -0300

    Inclusão de um validador de campos por inatividade
    Aqui foi feito um validador de campos por inatividade. Se o usuário não digitar nada em 10s, os campos serão limpos e uma mensagem aparecerá. Pelo que vi, meu código está completamente incorreto e fora do padrão. Estou criando temporizadores e cancelando eles. Em uma digitação normal, isso funciona. Mas em uma digitação mais rápida, execução de outras tarefas simultaneamente, isso se tornará um colapso em seu site.
    
    Pendente ajustes.

--- app/ex6/page.jsx
+++ app/ex6/page.jsx
@@ -5,37 +5,60 @@ import { useEffect, useState } from "react"
 export default function Func() {
 
     const [text, setText] = useState("");
-    const [CPF, setCPF] = useState("");
-    const [enviado, setEnviado] = useState(false);
+    const [Email, setEmail] = useState("");
+    const [Pass, setPass] = useState("");
 
     const Send = () => {
-        setEnviado(true);
+        if (Email.length === 0 || Pass.length === 0) {
+            setText("Preencha os campos sugeridos!");
 
-        console.log("BATEU!");
-    }
+            const id2 = setInterval(() => {
+                setText("");
+                clearInterval(id2);
+                }, 3000);
 
-    const ValNow = () => {
-        const id = setInterval(() => {
-            if (enviado === false) {
-                setCPF("");
-                setText("Campo limpo por segurança");
-                clearInterval(id);
-            }
-        }, 10000);
+            return () => clearInterval(id2);
+        }
+        else {
+            setEmail("");
+            setPass("");
 
-        return () => clearInterval(id);
+            alert("Dados enviados com sucesso!");
+        }
     }
 
     useEffect(() => {
-        ValNow();
-    }, [CPF]);
+        if (Email.length > 0 || Pass.length > 0) {
+            const id = setInterval(() => {
+                setEmail("");
+                setPass("");
+                setText("Campos limpos por segurança");
+                clearInterval(id);
+
+                const id2 = setInterval(() => {
+                setText("");
+                clearInterval(id2);
+                }, 3000);
+
+                return () => clearInterval(id2);
+
+            }, 10000);
+
+            return () => clearInterval(id);
+        }
+    }, [Email, Pass]);
 
     return (
         <div className="w-full h-dvh flex flex-col items-center">
             <div className="w-8/10 mt-70 flex flex-col gap-4 items-center justify-center">
-                <p className="text-3xl font-light">Digite seu CPF</p>
+                <p className="text-3xl font-light">Digite seu Email e Senha:</p>
 
-                <input className="p-2 text-center border-solid border-[#c9c9c9] border rounded-md" type="text" id="txtCPF" value={CPF} onChange={(e) => setCPF(e.target.value)}/>
+                <p className="text-2xl font-light">Email:</p>
+                <input className="p-2 w-80 text-center border-solid border-[#c9c9c9] border rounded-md" type="email" value={Email} onChange={(e) => setEmail(e.target.value)}/>
+
+                <p className="text-2xl font-light">Senha:</p>
+                <input className="p-2 w-80 text-center border-solid border-[#c9c9c9] border rounded-md" type="password" value={Pass} onChange={(e) => setPass(e.target.value)}/>
+
                 <Component text={text}/>
 
                 <button className="w-full text-black p-2 bg-yellow-300 hover:text-black hover:bg-yellow-400 hover:border-yellow-500 border-solid border-yellow-400 border-b-2 border-r-2 rounded-md" onClick={Send}>Enviar</button>
